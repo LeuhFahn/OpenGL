8735ef2cbf10e0a023816bec609d66078b23b3a0
LeuhFahn <leuh.fahn@gmail.com>
2013-12-26 18:06:27 +0100

    SDL

--- src/Tools/GLCommon.h
+++ src/Tools/GLCommon.h
@@ -1,3 +1,3 @@
 #define GLEW_STATIC
 #include "GLEW/glew.h"
-#include "glfw/glfw3.h"
+#include "SDL/SDL.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,124 +1,123 @@
+
+#include <GLEW/glew.h>
+#include <SDL/SDL.h>
 #include <iostream>
-#include "Tools/GLCommon.h"
-#include "Tools/ShaderGLSL.h"
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
 
-int main()
-{
-	int nWidth = 1024, nHeight = 768;
-	// Initializing GLFW
-    if( !glfwInit() )
+int main(int argc, char **argv)
+{	
+	 int nWidth = 1024, nHeight = 768;
+    // Notre fenêtre
+	
+    SDL_Window* window(0);
+    SDL_GLContext contexteOpenGL(0);
+	
+    SDL_Event evenements;
+    bool bQuit(false);
+	
+	
+    // Initialisation de la SDL
+	
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
+        std::cout << "Error init SDL : " << SDL_GetError() << std::endl;
+        SDL_Quit();
+		
+        exit( EXIT_FAILURE );
+    }
+	
+    // Version d'OpenGL
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+	
+	
+    // Double Buffer
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	
+	
+    // Création de la fenêtre
+    window = SDL_CreateWindow("Test SDL 2.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, nWidth, nHeight, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+
+    if(window == 0)
+    {
+        std::cout << "Error window creation: " << SDL_GetError() << std::endl;
+        SDL_Quit();
         exit( EXIT_FAILURE );
     }
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Création du contexte OpenGL
+    contexteOpenGL = SDL_GL_CreateContext(window);
+    if(contexteOpenGL == 0)
+    {
+		std::cout << SDL_GetError() << std::endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		exit( EXIT_FAILURE );
+    }
 
-	//Creating a window and context
-	GLFWwindow* window = glfwCreateWindow(nWidth, nHeight, "openGL", NULL, NULL);
-	//GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", glfwGetPrimaryMonitor(), NULL); //for fullscreen
-	if (!window)
-	{
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
 
-	//Making the OpenGL context current
-	glfwMakeContextCurrent(window);
-
-	glfwSetKeyCallback(window, key_callback);
-
-	glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK)
     {
-         fprintf( stderr, "Failed to initialize GLEW\n" );
-         exit( EXIT_FAILURE );
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		SDL_GL_DeleteContext(contexteOpenGL);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		exit( EXIT_FAILURE );
     }
 
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    // Vertices et coordonnées
+    float vertices[] = {-0.5, -0.5,   0.0, 0.5,   0.5, -0.5};
 
-	 // Try to load and compile shader
-    int status;
-    ShaderGLSL base_shader;
-    status = load_shader_from_file(base_shader, "../shaders/base.glsl", ShaderGLSL::VERTEX_SHADER | ShaderGLSL::FRAGMENT_SHADER);
-    if ( status == -1 )
+    // Boucle principale
+    while(!bQuit)
     {
-        fprintf(stderr, "Error on loading  shader/base.glsl\n");
-        exit( EXIT_FAILURE );
+        // Gestion des évènements
+		// Récupération d'un évènement
+		SDL_PollEvent(&evenements);
+	
+        if(evenements.window.event == SDL_WINDOWEVENT_CLOSE)
+            bQuit = true;
+
+		// Switch sur le type d'évènement
+		switch(evenements.type)
+		{
+			case SDL_KEYDOWN:
+        
+				// Gestion des touches
+				switch(evenements.key.keysym.scancode)
+				{
+					case SDL_SCANCODE_ESCAPE:
+						 bQuit = true;
+					break;
+				}
+
+			break;
+		}
+
+        // Nettoyage de l'écran
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // On remplie puis on active le tableau Vertex Attrib 0
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, vertices);
+        glEnableVertexAttribArray(0);
+
+
+        // On affiche le triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // On désactive le tableau Vertex Attrib puisque l'on n'en a plus besoin
+        glDisableVertexAttribArray(0);
+
+        // Actualisation de la fenêtre
+        SDL_GL_SwapWindow(window);
     }
 
-	//Geometrie
-/*	GLuint vbo;
-	glGenBuffers(1, &vbo);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    // On quitte la SDL
+    SDL_GL_DeleteContext(contexteOpenGL);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-	GLfloat vertices[] = { -0.5, -0.5, 1, 0, 0, 0.5, -0.5, 0, 1, 0, 0, 0.5, 0, 0, 1 };
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), 
-		vertices, GL_STATIC_DRAW);
-
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-
-	glBindVertexArray(vao);
-
-	glEnableVertexAttribArray(0);
-	// glVertexAttribPointer(index, size, type, normalized, stride, pointer)
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE,  5 * sizeof(GLfloat), 0 * sizeof(GLfloat));
-
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,  5 * sizeof(GLfloat), (const GLvoid*) (2 * sizeof(GLfloat)));
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-*/
-	float vertices[] = {-0.5, -0.5,   0.0, 0.5,   0.5, -0.5};
-
-	while (!glfwWindowShouldClose(window))
-	{
-		/*glViewport( 0, 0, nWidth, nHeight  );
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		// Bind gbuffer shader
-        glUseProgram(base_shader.program);
-
-		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glBindVertexArray(0);*/
-
-		glClear(GL_COLOR_BUFFER_BIT);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, vertices);
-		glEnableVertexAttribArray(0);
-
-
-		// On affiche le triangle
-
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
-		// On désactive le tableau Vertex Attrib puisque l'on en a plus besoin
-
-		glDisableVertexAttribArray(0);
-
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-	}
-/*
-	glDeleteBuffers(1, &vbo);
-	glDeleteVertexArrays(1, &vao);
-*/
-	//terminating GLFW
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	return 0;
+    return 0;
 }
